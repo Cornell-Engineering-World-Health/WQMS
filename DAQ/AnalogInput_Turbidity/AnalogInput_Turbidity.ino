5fe6755e7966fd9ab90334a0b18d5d114a5fe51d
Adam <abc248@cornell.edu>
2016-09-01 14:01:51 -0400

    Turbidity Sensor Arduino Code 9.1.2016

---
+++ DAQ/AnalogInput_Turbidity/AnalogInput_Turbidity.ino
@@ -0,0 +1,49 @@
+/*
+  Analog Input
+ Demonstrates analog input by reading an analog sensor on analog pin 0 and
+ turning on and off a light emitting diode(LED)  connected to digital pin 13.
+ The amount of time the LED will be on and off depends on
+ the value obtained by analogRead().
+
+ The circuit:
+ * Photocell attached to analog input 0
+ * center pin of the potentiometer to the analog pin
+ * one side pin (either one) to ground
+ * the other side pin to +5V
+ * LED anode (long leg) attached to digital output 13
+ * LED cathode (short leg) attached to ground
+
+ * Note: because most Arduinos have a built-in LED attached
+ to pin 13 on the board, the LED is optional.
+
+
+ Created by David Cuartielles
+ modified 30 Aug 2011
+ By Tom Igoe
+
+ This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/AnalogInput
+
+ */
+
+int sensorPin = A0;    // select the input pin for the photocell
+int ledPin = 8;      // select the pin for the LED
+
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(ledPin, OUTPUT);
+  pinMode(sensorPin, INPUT);
+}
+
+void loop() {
+  // turn the ledPin on
+  digitalWrite(ledPin, HIGH);
+  // read the value from the sensor
+  sensorValue = analogRead(sensorPin);
+  Serial.println(sensorValue);
+  // delay should synchronize with those of other probes
+  delay(500);
+}
